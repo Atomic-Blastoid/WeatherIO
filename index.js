1df5bd416e0451c9949d599a0a5a31afc1fa77fa
Raj Choithramani <122195811+Atomic-Blastoid@users.noreply.github.com>
2025-06-30 22:03:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-let weather={
-    apikey:"b891ecf66c32dc34cbfd9c399b3e5e98",
-    fetchWeather: function(city){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city+"&appid="+this.apikey+"&units=metric")
-        .then((Response)=>Response.json())
-        .then((data)=>this.displayWeather(data))
-    },displayWeather:function(data){
-        const {name}=data;
-        const {icon,description}=data.weather[0];
-        const{temp , humidity}=data.main;
-        const {speed}=data.wind;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
-        document.querySelector(".description").innerText =description ;
-        document.querySelector(".temp").innerText =temp+"°C" ;
-        document.querySelector(".humidity").innerText ="Humidity: "+humidity+"%";
-        document.querySelector(".wind").innerText ="Wind speed: "+speed+"km/hr";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage="url('https://source.unsplash.com/1550x900/?"+ name +"')";
-        
-    },
-    search:function(){
-        this.fetchWeather(document.querySelector(".search-bar").value);
+let weather = {
+  apikey: "b891ecf66c32dc34cbfd9c399b3e5e98",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apikey + "&units=metric")
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    if (data.cod !== 200) {
+      alert("City not found!");
+      return;
     }
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+
+    document.body.style.backgroundImage = "url('https://picsum.photos/1600/900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
 };
-document.querySelector(".search button")
-.addEventListener("click",function(){
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
     weather.search();
-
-});
-document.querySelector(".search-bar").addEventListener("keyup",function(event){
-    if(event.key==="Enter"){
-        weather.search();
-    }
-
+  }
 });
 
-
-
+// ✅ Load default city
+weather.fetchWeather("Ranchi");
